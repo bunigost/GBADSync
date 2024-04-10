73d15de3a179cf3f8d31b2389c50b91835a60530
PedroPerez14 <756642@unizar.es>
2024-04-10 23:07:37 +0200

    Add modified citra.h and compilation dependencies
    One of the dependencies isn't deprecated or hard to find yet, but I'm including it anyway

--- source/modules/citra.h
+++ source/modules/citra.h
@@ -10,15 +10,25 @@
 
 #include "dropbox.h"
 
-std::string getUpdateTimestampForCitraSave(std::string dropboxToken, std::string fullPath) {
+std::vector<std::string> getUpdateTimestampsForCitraSave(std::string dropboxToken, std::string fullPath, std::vector<std::string>& filenames) {
     Dropbox dropbox(dropboxToken);
+    std::vector<std::string> strings;
+    strings.empty();
+    filenames.empty();
     auto results = dropbox.list_folder(fullPath + "/data/00000001");
     for (auto lr : results) {
-        if (lr.name == "user1") {
-            return lr.server_modified;
+        if (lr.name.substr(0, 4) == "user" || lr.name.substr(0, 6) == "system") {
+            // filenames will store the names of the user<number> and system files, if any are found
+            // This vector will be used by downloadCitraSavesToCheckpoint() to download all the save slots with valid timestamp
+            // instead of only the first one, like up until now
+            filenames.push_back(lr.name);
+            strings.push_back(lr.server_modified);
+        }
+        else {
+            std::cout << lr.name << ": Could not find timestamp, skipping" << std::endl; 
         }
     }
-    return "";
+    return strings;
 }
 
 std::string checkpointDirToCitraGameCode(std::string checkpointGameSaveDir) {
@@ -56,11 +66,11 @@ std::map<std::string, std::string> findCheckpointSaves(std::string checkpointPat
             }
         }
     }
-
     return pathmap;
 }
 
-void downloadCitraSaveToCheckpoint(std::string dropboxToken, std::string timestamp, std::string checkpointPath, std::map<std::string, std::string> pathmap, std::string gameCode, std::string fullPath) {
+void downloadCitraSavesToCheckpoint(std::string dropboxToken, std::string timestamp, std::string checkpointPath, 
+                        std::map<std::string, std::string> pathmap, std::string gameCode, std::string fullPath, std::vector<std::string> saveFileNames) {
     Dropbox dropbox(dropboxToken);
 
     std::string baseSaveDir = checkpointPath + "/saves";
@@ -95,29 +105,32 @@ void downloadCitraSaveToCheckpoint(std::string dropboxToken, std::string timesta
         std::cout << "Checkpoint save dir already exists: " << destPath << std::endl;
     }
 
-    dropbox.download(fullPath + "/data/00000001/system",  destPath + "/system");
-    dropbox.download(fullPath + "/data/00000001/user1", destPath + "/user1");
+    for (std::string fileName : saveFileNames) {
+        dropbox.download(fullPath + "/data/00000001/" + fileName,  destPath + "/" + fileName);
+    }
+
 }
 
 void downloadCitraSaves(std::string dropboxToken, std::string checkpointPath) {
     auto pathmap = findCheckpointSaves(checkpointPath);
 
+    // This vector will have the same size as timestamps after calling getUpdateTimestampsForCitraSave()!
+    std::vector<std::string> saveFileNames;
+
     Dropbox dropbox(dropboxToken);
     auto folder = dropbox.list_folder("/sdmc/Nintendo 3DS/00000000000000000000000000000000/00000000000000000000000000000000/title/00040000");
     for (auto lr : folder) {
-        std::string timestamp = getUpdateTimestampForCitraSave(dropboxToken, lr.path_display);
-        if (timestamp == "") {
-            std::cout << lr.name << ": Could not find timestamp, skipping" << std::endl; 
-            continue;
-        }
+        std::vector<std::string> timestamps = getUpdateTimestampsForCitraSave(dropboxToken, lr.path_display, saveFileNames);
 
-        downloadCitraSaveToCheckpoint(
-            dropboxToken,
-            timestamp,
-            checkpointPath,
-            pathmap,
-            lr.name,
-            lr.path_display
+        downloadCitraSavesToCheckpoint(
+        dropboxToken,
+        timestamps.at(0),       // Now this is a vector
+        checkpointPath,
+        pathmap,
+        lr.name,
+        lr.path_display,
+        saveFileNames           // Can be "user<number>" or "system"
         );
+
     }
 }
